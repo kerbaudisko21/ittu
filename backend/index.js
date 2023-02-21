9f8e527af6ff97149445d9a9b5b1318b8821c6ff
Wilbert <Wilbertw99@gmail.com>
2023-02-21 17:58:43 +0700

    first commit

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+mongoose.set('strictQuery', true);
+const app = express();
+dotenv.config();
+
+const connect = async ()=>{
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to MongoDB")
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on("disconnected",()=>{
+    console.log("mongo disconnected")
+})
+
+mongoose.connection.on("connected",()=>{
+    console.log("mongo connected")
+})
+
+
+
+app.listen(8800, ()=>{
+    connect();
+    console.log("Connected to backend!");
+})
