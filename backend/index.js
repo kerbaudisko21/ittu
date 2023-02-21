6f41f90a2c96f967d3635b9cf34c16d69fc6aa23
RaymonddC <raymondchrisandy@gmail.com>
2023-02-21 18:34:58 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,29 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 const app = express();
 dotenv.config();
 
-const connect = async ()=>{
-    try {
-        await mongoose.connect(process.env.MONGO);
-        console.log("Connected to MongoDB")
-    } catch (error) {
-        throw error;
-    }
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    throw error;
+  }
 };
 
-mongoose.connection.on("disconnected",()=>{
-    console.log("mongo disconnected")
-})
+mongoose.connection.on("disconnected", () => {
+  console.log("mongo disconnected");
+});
 
-mongoose.connection.on("connected",()=>{
-    console.log("mongo connected")
-})
+mongoose.connection.on("connected", () => {
+  console.log("mongo connectede");
+});
 
-
-
-app.listen(8800, ()=>{
-    connect();
-    console.log("Connected to backend!");
-})
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend!");
+});
