23dfbb6aaae00ca946c7f3bf2cf54d96fdc393cf
RaymonddC <raymondchrisandy@gmail.com>
2023-03-09 22:44:08 +0700

    route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from './pages/home/Home';
-import List from './pages/list/List';
-import Schedule from './pages/schedule/Schedule';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Schedule from "./pages/schedule/Schedule";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/list" element={<List />} />
         <Route path="/list/:id" element={<Schedule />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
