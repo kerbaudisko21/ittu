eac70fdadaf66875e04ccc363fae3818fae27b1d
Gary <marvelimgary@gmail.com>
2023-04-26 21:17:46 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch GM
    Your branch is up to date with 'origin/GM'.
    
    Changes to be committed:
    	modified:   frontend/package-lock.json
    	modified:   frontend/package.json
    	modified:   frontend/src/App.js
    	modified:   frontend/src/pages/list/List.jsx
    	modified:   frontend/src/pages/list/list.css
    	modified:   frontend/src/pages/listTest/listTest.jsx
    	new file:   frontend/src/pages/listTest/listTest2.jsx
    	new file:   frontend/src/pages/listTest/listTest3.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import Home from './pages/home/Home';
 import List from './pages/list/List';
 import Schedule from './pages/schedule/Schedule';
 import Login from './pages/login/Login';
+import ListTest from './pages/listTest/listTest';
+import ListTest2 from './pages/listTest/listTest2';
+import ListTest3 from './pages/listTest/listTest3';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/list" element={<List />} />
+        <Route path="/listtest" element={<ListTest />} />
+        <Route path="/listtest2" element={<ListTest2 />} />
+        <Route path="/listtest3" element={<ListTest3 />} />
         <Route path="/list/:id" element={<Schedule />} />
         <Route path="/login" element={<Login isLogin={true} />} />
         <Route path="/register" element={<Login isLogin={false} />} />
