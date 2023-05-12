0e6818b3f78a97d5eff8785c30da51945497d5fb
Gary <marvelimgary@gmail.com>
2023-05-12 21:12:36 +0700

    BEBERESAN

--- frontend/src/pages/list/List.jsx
+++ frontend/src/pages/list/List.jsx
@@ -1,18 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import "./list.css";
-// import MapTempat from "../../components/map/MapTempat.jsx";
-// import PlaceMap from "../../components/PlaceMap/PlaceMap";
-import { GoogleMap, useLoadScript,Marker } from "@react-google-maps/api";
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+
+import { GoogleApiWrapper } from 'google-maps-react';
 import GoogleMapReact from 'google-map-react';
-import {Grid} from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import axios from "axios";
 import { useLocation } from "react-router-dom";
 import { Autocomplete } from '@react-google-maps/api';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import uuid from 'uuid/v4';
 import { v4 as uuidv4 } from 'uuid';
+
 
 
 
@@ -24,16 +21,7 @@ import DndStore from '../listTest/DndStore';
 
 
 // a little function to help us with reordering the result
-const reorder = (list, startIndex, endIndex) => {
-  console.log("reorder")
-  const result = Array.from(list);
-  console.log(result)
-  const [removed] = result.splice(startIndex, 1);
-  console.log([removed])
-  result.splice(endIndex, 0, removed);
 
-  return result;
-};
 
 
 
@@ -60,77 +48,16 @@ const getListStyle = (isDraggingOver) => ({
 
 
 
-
-
-
-
-
-
-
-
-
-
 const List = (props) => {
 
   const [autocomplete, setAutocomplete] = useState(null);
   const dateRange = useLocation();
   const { startDate, endDate,  name,latitude, longitude } = dateRange.state;
-  const [dragging, setDragging] = useState(false);
-  
   const [stores, setStores] = useState([]);
   const [location, setLocation] = useState({});
-  const [storeList, setStoreList] = useState([]);
-
   const [type, setType] = useState('');
-
- 
-
-  console.log(dateRange.state)
-  console.log(name)
-  console.log(latitude)
-  console.log(longitude)
-  // const [startDate, setStartDate] = useState("");
-  // const [endDate, setEndDate] = useState("");
   const [ItineraryDay, setItineraryDay] = useState([]);
   
-  // console.log(startDate)
-  // console.log(endDate)
-  
-  async function getWeatherForecast(lat, lon, date) {
-    const apiKey = 'ab153f1fe2b71c6c9649d3beaeefc00c';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    
-    // Filter the list based on the date you want
-    const filteredList = data.list.filter(item => {
-      const itemDate = new Date(item.dt_txt);
-      return itemDate.toDateString() === new Date(date).toDateString();
-    });
-    return filteredList;
-  }
-  
-  async function addWeatherToItinerary(itinerary) {
-    for (let i = 0; i < itinerary.length; i++) {
-      const date = itinerary[i].date;
-      const lat = 44.34;
-      const lon = 10.99;
-      const weatherData = await getWeatherForecast(lat, lon, date);
-      itinerary[i].weather = weatherData[0].weather[0].description;
-      itinerary[i].temperature = weatherData[0].main.temp;
-    }
-    
-    setItineraryDay(itinerary);
-  }
-
-
-  const handleTypeChange = (event) => {
-    setType(event.target.value);
-    console.log(event.target.value)
-  };
-
-
-
   useEffect(() => {
     function getDates() {
       const dates = [];
@@ -168,12 +95,8 @@ const List = (props) => {
     getDates();
   }, [startDate, endDate]);
 
-
-
-
-  console.log(storeList)
-
   useEffect(() => {
+    // Get Current Position
     // navigator.geolocation.getCurrentPosition(
     //   (position) => {
     //     const { latitude, longitude } = position.coords;
@@ -188,7 +111,6 @@ const List = (props) => {
     
   }, [latitude,longitude]);
  
-  
   useEffect(() => {
     const { google } = props;
     const service = new google.maps.places.PlacesService(
@@ -212,112 +134,46 @@ const List = (props) => {
     
     service.nearbySearch(request, callback);
   }, [props, location, type]);
-
-
-  const renderList = () => {
-    console.log(stores)
-    return stores.map((store) => 
-
-    <div
-    id={store.place_id}
-    draggable="true"
-    onDragStart={handleDragStart}
-    onDragEnd={handleDragEnd}
-  >
-  {store.name} 
-  </div>
-   
-    )
-  };
-   
   
 
-
-  const handleClick = (data) => {
-    const newStore = {
-      name: data,
-      lat: data.lat,
-      lng: data.lng
-    };
-    setStoreList((storeList) => [...storeList, newStore]);
+  async function addWeatherToItinerary(itinerary) {
+    for (let i = 0; i < itinerary.length; i++) {
+      const date = itinerary[i].date;
+      const lat = latitude;
+      const lon = longitude;
+      const weatherData = await getWeatherForecast(lat, lon, date);
+      itinerary[i].weather = weatherData[0].weather[0].description;
+      itinerary[i].temperature = weatherData[0].main.temp;
+    }
     
+    setItineraryDay(itinerary);
+  }
+  
+  async function getWeatherForecast(lat, lon, date) {
+    const apiKey = 'ab153f1fe2b71c6c9649d3beaeefc00c';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    
+    // Filter the list based on the date you want
+    const filteredList = data.list.filter(item => {
+      const itemDate = new Date(item.dt_txt);
+      return itemDate.toDateString() === new Date(date).toDateString();
+    });
+    return filteredList;
+  }
+  
+
+  const handleTypeChange = (event) => {
+    setType(event.target.value);
+    console.log(event.target.value)
   };
-  // console.log(storeList);
 
   const handleChange = ({center}) => {
     setLocation( {latitude: center.lat,
       longitude: center.lng} );
 
-      // console.log(location)
   }
-
-  console.log(ItineraryDay)
-
-  const handleDragStart = (event) => {
-    setDragging(true);
-    event.dataTransfer.setData('text/plain', event.target.id);
-
-  };
-
-  const handleDragEnd = () => {
-    setDragging(false);
-  };
-
-
-  const handleDrop = (e, id) => {
-    // Nanti tambahin id, lat, long dll
-    console.log(e)
-    e.preventDefault();
-    const itemId = e.dataTransfer.getData("text/plain");
-    console.log(itemId)
-
-    if (typeof stores.find((item) => item.place_id === itemId) === "undefined") {
-      console.log("The variable is undefined");
-      const newStore = {
-        name: itemId,
-      };
-
-      const newDestinations = [...ItineraryDay[id - 1].destinations, newStore];
-
-      setItineraryDay((prevData) =>
-        prevData.map((Itinerary) =>
-          Itinerary.id === id ? { ...Itinerary, destinations: newDestinations} : Itinerary
-        )
-      );
-      
-
-    } else {
-
-      console.log("The variable is defined");
-
-      const destinationName = stores.find((item) => item.place_id === itemId).name;
-      const destinationId = stores.find((item) => item.place_id === itemId).place_id;
-
-      const newStore = {
-        name: destinationName,
-        id: destinationId
-      };
-  
-      const newDestinations = [...ItineraryDay[id - 1].destinations, newStore];
-     
-      console.log(newStore)
-  
-      setItineraryDay((prevData) =>
-        prevData.map((Itinerary) =>
-          Itinerary.id === id ? { ...Itinerary, destinations: newDestinations} : Itinerary
-        )
-      );
-    }
-
-    
-   
-  };
-
-
-  const handleDragOver = (event) => {
-    event.preventDefault();
-  };
-
 
   const onLoad = (autoC) => setAutocomplete(autoC);
 
@@ -329,7 +185,13 @@ const List = (props) => {
   };
 
 
-
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+  
+    return result;
+  };
 
   const onDragEnd = (result) => {
     // dropped outside the list
@@ -464,7 +326,7 @@ const List = (props) => {
      <h1>{name}</h1> 
       <div>
       
-      <Droppable droppableId="droppable" type="droppableItem" >
+      <Droppable droppableId="droppable" type="droppableItem">
         {(provided, snapshot) => (
           <div
             ref={provided.innerRef}
@@ -486,16 +348,6 @@ const List = (props) => {
                       {item.date.toDateString()}
                       <p>Weather: {item.weather}</p>
                      <p>Temperature: {item.temperature}</p>
-                      <span
-                        {...provided.dragHandleProps}
-                        style={{
-                          display: "inline-block",
-                          margin: "0 10px",
-                          border: "1px solid #000"
-                        }}
-                      >
-                        Drag
-                      </span>
                       <ServiceCommandUnit
                         destinations={item.destinations}
                         type={item.id}
@@ -512,79 +364,12 @@ const List = (props) => {
       </Droppable>
    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      {ItineraryDay.map(day => {
-        return (
-          <div key={day.id}
-          
-          >
-            <h2>{day.date.toDateString()}</h2>
-            <p>Weather: {day.weather}</p>
-            <p>Temperature: {day.temperature}</p>
-            <div
-                  id="droppable1"
-                  onDrop={(e) => handleDrop(e, day.id)}
-                  onDragOver={handleDragOver}
-                  style={{
-                    backgroundColor: dragging ? 'lightgray' : 'white',
-                    flexGrow: '1',
-                    textAlign: 'left',
-                    lineHeight: '50px',
-                  }}
-                  
-                > 
-                Destination :
-                
-                </div>
-                  
-                {day.destinations.map((destination) => (
-                  <div
-                  id={destination.name}
-                  draggable="true"
-                  onDragStart={handleDragStart}
-                  onDragEnd={handleDragEnd}
-
-                > 
-                  {destination.name}
-                  
-                </div>
-             
-              ))}
-
-
-
-
-
-          </div>
-        );
-      })}
+    
     </div>
-   
-      
-      
-       
   
       </div>
-
-
       <div className="placemap">
-        {/* <PlaceMap /> */}
+   
       
       <p>Latitude: {location.latitude}</p>
       <p>Longitude: {location.longitude}</p>
@@ -601,50 +386,8 @@ const List = (props) => {
      
       <DndStore stores={stores} />
     
-      {/* <Droppable droppableId="Stores" type={`droppableSubItem`} isDropDisabled={true}>
-      {(provided, snapshot) => (
-        <div
-          ref={provided.innerRef}
-          {...provided.droppableProps}
-          style={getListStyle(snapshot.isDraggingOver)}
-        >
-          {stores.map((item, index) => (
-            <Draggable key={item.place_id} draggableId={item.place_id} index={index}>
-              {(provided, snapshot) => (
-                <>
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    style={getItemStyle(
-                      snapshot.isDragging,
-                      provided.draggableProps.style
-                    )}
-                  >
-                    {item.name}
-                    <span
-                      {...provided.dragHandleProps}
-                      style={{
-                        display: "block",
-                        margin: "0 10px",
-                        border: "1px solid #000"
-                      }}
-                    >
-                      Drag
-                    </span>
-                  </div>
-                  {provided.placeholder}
-                </>
-              )}
-            </Draggable>
-          ))}
-          {provided.placeholder}
-        </div>
-      )}
-    </Droppable> */}
 
-    <ul>{renderList()}</ul>
       </div>
-      
       </DragDropContext>
       <div className="MapTempat">
       <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}
@@ -671,7 +414,6 @@ const List = (props) => {
             lat={store.geometry.location.lat()}
             lng={store.geometry.location.lng()}
             text={store.name}
-            onClick={() => handleClick(store.name)}
             >            
             <LocationOnOutlinedIcon
             color="primary" fontSize="large" 
@@ -691,3 +433,4 @@ const List = (props) => {
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyDmowFuG5A64eipfP8pOHIh0v4onGzDKYk',
 })(List);
+
