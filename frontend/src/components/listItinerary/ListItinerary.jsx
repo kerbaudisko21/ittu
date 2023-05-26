122ab6bd5961c5a45167430291684aa1fbb6a85a
Gary <marvelimgary@gmail.com>
2023-05-26 17:03:22 +0700

    Masih Bug Dikit

--- frontend/src/components/listItinerary/ListItinerary.jsx
+++ frontend/src/components/listItinerary/ListItinerary.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Droppable, Draggable } from 'react-beautiful-dnd';
 import { v4 as uuidv4 } from 'uuid';
-import { FaCaretDown, FaCaretRight} from 'react-icons/fa';
+import { FaCaretDown, FaCaretRight } from 'react-icons/fa';
 import { useCollapse } from 'react-collapsed';
 
 
@@ -11,60 +11,84 @@ import ListItineraryDestination from '../listItineraryDestination/ListItineraryD
 
 
 
-const ListItinerary = ({ItineraryDay,setItineraryDay,response,updateOptions}) => {
+const ListItinerary = ({ ItineraryDay, setItineraryDay, response, updateOptions }) => {
 
-    const updateArray = (index, value) => {
-        let Itinerary = [...ItineraryDay];
-        value = value.map((item) => ({ ...item, id: uuidv4()}))
-    
-        Itinerary = Itinerary.map((item) => {
-          if (item.id === index) {
-      
-            item.destinations = value;
-          }
-          return item;
-        });
-        setItineraryDay(Itinerary);
-      };
+  console.log(ItineraryDay)
 
-      const deleteItem = (deleteId,type) => {
-      
-        console.log(deleteId)
-        console.log(type)
-  
-        let newItinerary ;
-  
-        newItinerary = ItineraryDay.map((item) => {
-          console.log(item)
-          if (item.id === type) {
-          const newDestination = [...item.destinations];
-          newDestination.splice(deleteId, 1);
-          console.log(newDestination)
-          item.destinations = newDestination
-          }
-          return item;
-        });
-        
-       setItineraryDay(newItinerary) 
+  const updateArray = (index, value) => {
+    let Itinerary = [...ItineraryDay];
+    value = value.map((item) => ({ ...item, id: uuidv4() }))
+
+    Itinerary = Itinerary.map((item) => {
+      if (item.id === index) {
+
+        item.destinations = value;
       }
+      return item;
+    });
+    setItineraryDay(Itinerary);
+  };
 
-      const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
+  const deleteItem = (deleteId, type) => {
+
+    console.log(deleteId)
+    console.log(type)
+
+    let newItinerary;
+
+    newItinerary = ItineraryDay.map((item) => {
+      console.log(item)
+      if (item.id === type) {
+        const newDestination = [...item.destinations];
+        newDestination.splice(deleteId, 1);
+        console.log(newDestination)
+        item.destinations = newDestination
+      }
+      return item;
+    });
+
+    setItineraryDay(newItinerary)
+  }
+
+  const Collapsible = ({children , item}) => {
+    const [isExpanded, setExpanded] = useState(false);
+    const { getCollapseProps, getToggleProps} = useCollapse(
+      { 
+        isExpanded : item.expandedState 
+      },
+    );
+
+    function handleOnClick() {
+      item.expandedState = !isExpanded;
+      setExpanded(!isExpanded); 
+     }
+  
+    return (
+      <div className="list-test">
+        <div {...getToggleProps({onClick: handleOnClick})}>
+        <div className='ItineraryTop'>
+        {isExpanded ? <FaCaretDown /> : <FaCaretRight />}
+        <h2 className='ItineraryDate'>{item.date.toDateString()}</h2>
+        <img className="ItineraryIcon" src={`http://openweathermap.org/img/w/${item.icon}.png`} alt='icon' />
+          </div>
+        </div>
+        <div {...getCollapseProps()}>
+       {children}
+        </div>
+      </div>
+    );
+  };
 
   return (
-    <div className="collapsible">
-        <div {...getToggleProps()}>
-        {isExpanded ? <FaCaretDown /> : <FaCaretRight/>}
-      </div>
-      <div {...getCollapseProps()}>
-        <div className="content">
+
     <div className='listContainer'>
-    <Droppable droppableId="droppable" type="droppableItem">
+      <Droppable droppableId="droppable" type="droppableItem">
         {(provided) => (
           <div
             ref={provided.innerRef}
             {...provided.droppableProps}
             className='itineraryContainer'
-           
+
           >
             {ItineraryDay.map((item, index) => (
               <Draggable key={item.id} draggableId={item.id} index={index}>
@@ -73,25 +97,27 @@ const ListItinerary = ({ItineraryDay,setItineraryDay,response,updateOptions}) =>
                     <div
                       ref={provided.innerRef}
                       {...provided.draggableProps}
-                        className='tripContainer'
+                      className='tripContainer'
                     >
-                      <div className='ItineraryTop'>
-                      <h2 className='ItineraryDate'>{item.date.toDateString()}</h2>
-                      <img className="ItineraryIcon" src={`http://openweathermap.org/img/w/${item.icon}.png`}  alt='icon'/>
-                      </div>
+                      <Collapsible item={item}>
+                        
                       <p>Weather: {item.weather}</p>
-                      <p>Temperature: {item.temperature}</p>
-                      <ListItineraryDestination
-                        destinations={item.destinations}
-                        type={item.id}
-                        addPlace={updateArray}
-                        showDirection={updateOptions}
-                        response={response}
-                        deleteItem={deleteItem}
-                      />                          
+                          <p>Temperature: {item.temperature}</p>
+                          <ListItineraryDestination
+                            destinations={item.destinations}
+                            type={item.id}
+                            addPlace={updateArray}
+                            showDirection={updateOptions}
+                            response={response}
+                            deleteItem={deleteItem}
+                          />
+                     
+                      </Collapsible>
                     </div>
+                    
+
                   </>
-                
+
                 )}
               </Draggable>
             ))}
@@ -99,10 +125,7 @@ const ListItinerary = ({ItineraryDay,setItineraryDay,response,updateOptions}) =>
           </div>
         )}
       </Droppable>
-      </div>
-      </div>
-      </div>
-      </div>
+    </div>
   )
 }
 
