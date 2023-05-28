f647b20da94148ec42d134c78738c3aefacbc1b8
Gary <marvelimgary@gmail.com>
2023-05-28 15:42:56 +0700

    Checklist start

--- frontend/src/components/listCheckList/ListCheckList.jsx
+++ frontend/src/components/listCheckList/ListCheckList.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const ListCheckList = () => {
+import './listCheckList.css'
+
+function ListCheckList() {
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = () => {
+    setTodos([...todos, { text: '', completed: false }]);
+  };
+
+  const handleTodoChange = (event, index) => {
+    const newTodos = [...todos];
+    newTodos[index].text = event.target.value;
+    setTodos(newTodos);
+  };
+
+  const handleCheckboxChange = (event, index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = event.target.checked;
+    setTodos(newTodos);
+  };
+
+  const handleTodoBlur = (event, index) => {
+    const newTodos = [...todos];
+    if (event.target.value === '') {
+      newTodos.splice(index, 1);
+      setTodos(newTodos);
+    }
+  };
+
   return (
-    <div>ListCheckList</div>
-  )
+    <div>
+    <h2 className='listCheckTitle'>Check List</h2>
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index} style={{ listStyleType: 'none' }}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={(event) => handleCheckboxChange(event, index)}
+            />
+            <input
+              type="text"
+              value={todo.text}
+              onChange={(event) => handleTodoChange(event, index)}
+              onBlur={(event) => handleTodoBlur(event, index)}
+              style={{
+                border: 'none',
+                textDecoration: todo.completed ? 'line-through' : 'none',
+
+              }}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default ListCheckList
+export default ListCheckList;
